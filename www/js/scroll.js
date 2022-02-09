0546e40c591b4719406c6b87a83fad3f6baefd2b
miikaok <miika.ojakaukola@icloud.com>
2022-02-09 12:17:15 +0200

    Added functionality to sidedrawer

--- www/js/scroll.js
+++ www/js/scroll.js
@@ -1,3 +1,5 @@
+let drawerState = false;
+
 scrollToAnchor = (id) => {
   console.log(id);
   $("html, body").animate(
@@ -7,3 +9,31 @@ scrollToAnchor = (id) => {
     600
   );
 };
+
+showDrawer = () => {
+  if (drawerState) {
+    $(".side__drawer").animate(
+      {
+        width: "0",
+      },
+      500
+    );
+    // $(".side__drawer").css("display", "none");
+    drawerState = false;
+  } else {
+    $(".side__drawer").animate(
+      {
+        width: "200px",
+      },
+      500
+    );
+    // $(".side__drawer").css("display", "inline");
+    drawerState = true;
+  }
+};
+
+$(window).resize(() => {
+  if ($(document).innerWidth() >= 703 && drawerState == true) {
+    showDrawer();
+  }
+});
